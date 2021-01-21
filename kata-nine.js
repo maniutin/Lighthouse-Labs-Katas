d652ce98b529680654911a615ab0ba13324d84ea
Nick Maniutin <maniutin@gmail.com>
2021-01-21 01:38:50 +0000

    solving kata niene

---
+++ kata-nine.js
@@ -0,0 +1,54 @@
+const PI = 3.14159;
+
+// Sphere Volume
+const sphereVolume = function (radius) {
+  return (4 / 3) * PI * Math.pow(radius, 3);
+};
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+// Cone Volume
+const coneVolume = function (radius, height) {
+  return PI * Math.pow(radius, 2) * (height / 3);
+};
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+// Prism Volume
+const prismVolume = function (height, width, depth) {
+  return width * depth * height;
+};
+console.log(prismVolume(3, 4, 5) === 60);
+
+// Total Volume
+const totalVolume = function (solids) {
+  let total = 0;
+  for (const solid of solids) {
+    if (solid.type === "sphere") {
+      total += sphereVolume(solid.radius);
+    } else if (solid.type === "cone") {
+      total += coneVolume(solid.radius, solid.height);
+    } else if (solid.type === "prims") {
+      total += prismVolume(solid.height, solid.width, solid.depth);
+    }
+  }
+  return total;
+};
+
+const largeSphere = {
+  type: "sphere",
+  radius: 40,
+};
+
+const smallSphere = {
+  type: "sphere",
+  radius: 10,
+};
+
+const cone = {
+  type: "cone",
+  radius: 3,
+  height: 5,
+};
+
+const duck = [largeSphere, smallSphere, cone];
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
