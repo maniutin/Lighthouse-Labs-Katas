949c5991bd6418a444f46dd27a544e46bb256480
Nick Maniutin <maniutin@gmail.com>
2021-01-23 22:33:17 +0000

    solving kata 12

---
+++ kata-12.js
@@ -0,0 +1,29 @@
+const organizeInstructors = function (instructors) {
+  let courses = {};
+  for (const instructor of instructors) {
+    if (!courses[instructor.course]) {
+      courses[instructor.course] = [];
+      courses[instructor.course].push(instructor.name);
+    } else {
+      courses[instructor.course].push(instructor.name);
+    }
+  }
+  return courses;
+};
+
+console.log(
+  organizeInstructors([
+    { name: "Samuel", course: "iOS" },
+    { name: "Victoria", course: "Web" },
+    { name: "Karim", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  organizeInstructors([
+    { name: "Brendan", course: "Blockchain" },
+    { name: "David", course: "Web" },
+    { name: "Martha", course: "iOS" },
+    { name: "Carlos", course: "Web" },
+  ])
+);
