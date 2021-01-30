8b7b98969949e26459cc72d6bbc55a5918f64157
Nick Maniutin <maniutin@gmail.com>
2021-01-30 02:59:03 +0000

    solving kata 15

---
+++ kata-15.js
@@ -0,0 +1,31 @@
+const squareCode = function (message) {
+  const lineLength = Math.ceil(Math.sqrt(message.replace(/ /g, "").length));
+  let arrColumns = [];
+  for (let i = 0; i < lineLength; i++) {
+    arrColumns.push(
+      message.replace(/ /g, "").slice(i * lineLength, lineLength * (i + 1))
+    );
+  }
+  let finalArr = [];
+  let arr = [1, 2, 3];
+  arrColumns.forEach((line, index) => {
+    for (let i = 0; i < line.length; i++) {
+      if (index === 0) {
+        finalArr.push(line[i]);
+      } else {
+        finalArr[i] += line[i];
+      }
+    }
+  });
+
+  return finalArr.join(" ");
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(
+  squareCode(
+    "if man was meant to stay on the ground god would have given us roots"
+  )
+);
